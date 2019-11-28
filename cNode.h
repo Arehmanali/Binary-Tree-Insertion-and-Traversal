d952973e38e7af6cddd66877e91461a0b7ec8a31
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2019-11-28 21:46:07 +0500

    IMPROVE

--- cNode.h
+++ cNode.h
@@ -6,11 +6,11 @@ class cNode
 public:
 	cNode* rightChild;		// point the address of right Child
 	cNode* leftChild;		// point the address of left Child
-	cNode();
-	cNode(int d);
-	void setData(int d);
-	int getData();
-	void print()const;
-	~cNode();
+	cNode();			// default constructor
+	cNode(int d);			// parametric constructor
+	void setData(int d);		// setter function for setting the data of node
+	int getData();			// getter function for getting the data of nodes
+	void print()const;		// print function for printing the data of nodes
+	~cNode();			// destructor
 };
 
