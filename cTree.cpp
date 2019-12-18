d0aeae54d4dfcd4cb8e80a8835dfe7d79f4c2409
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2019-12-18 22:33:03 +0500

    Update cTree.cpp

--- cTree.cpp
+++ cTree.cpp
@@ -58,47 +58,6 @@ cTree& cTree::insert(cNode*& ptr)
 	ptr = NULL;
 	count++;
 	return *this;
-	
-
-
-	/*if (!root)
-	{
-		root = ptr;
-		root->leftChild = NULL;
-		root->rightChild = NULL;
-		ptr = NULL;
-	}
-	else
-	{
-		cNode* current = root;
-		if (current->getData() < ptr->getData())
-		{
-				while (ptr->getData()<current->getData())
-				{
-					current = current->rightChild;
-				}
-				current->rightChild = ptr;
-				current = current->rightChild;
-				current->rightChild = current->rightChild = ptr = NULL;
-				//current->rightChild->rightChild =current->rightChild->leftChild= NULL;
-				//ptr = NULL;
-		}
-		else
-			{
-				while (ptr->getData() > current->getData())
-				{
-					current = current->leftChild;
-				}
-				current->leftChild = ptr;
-				current = current->leftChild;
-				current->leftChild = current->rightChild = ptr = NULL;
-				//current->leftChild->leftChild=current->leftChild->rightChild = NULL;
-				//ptr = NULL;
-			}
-	}
-	count++;
-	return *this;
-	*/
 }
 
 
